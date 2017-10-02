c426896e703bb46522e57d9006bea3b661d51d5e
dbdydgur2244 <dbdydgur2244@gmail.com>
2017-10-02 09:36:26 -0700

    modifying system call exec's function name

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -14,41 +14,69 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
-    int syscallnum;
-
-    printf ("system call!\n");
-
-    syscallnum = f->esp;
+void syscall_get_args(void *esp, void *args[], int syscallnum){
+    
     switch(syscallnum){
-        /* jimin */
         case SYS_HALT:
             break;
         case SYS_EXIT:
-            break;
-        case SYS_HALT:
-            break;
-        case SYS_EXIT:
+            args[0] =  (char *)esp + 4;
             break;
         case SYS_EXEC:
+            args[0] = (char *)esp + 4;
             break;
-        /* yonghyuk */
         case SYS_WAIT:
+            args[0] = (char *)esp + 4;
             break;
         case SYS_READ:
-            sysca_read();
+            args[0] = (char *)esp + 4;
+            args[1] = (char *)esp + 8;
+            args[2] = (char *)esp + 12;
             break;
         case SYS_WRITE:
-            syscall_write();
+            args[0] = (char *)esp + 4;
+            args[1] = (char *)esp + 8;
+            args[2] = (char *)esp + 12;
             break;
         default:
             break;
     }
+}
 
+/* YH updated..... */
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
+{
+    int syscallnum;
+    void *args[4];
+    printf ("system call!\n");
+    syscall_get_args()
+    syscallnum = *((int *)(f->esp));
+    switch(syscallnum){
+        /* jimin */
+        case SYS_HALT:
+            syscall_halt();
+            break;
+        case SYS_EXIT:
+            syscall_exit(*(int *)args[0]);
+            break;
+        case SYS_EXEC:
+            syscall_exec((char *)args[0]);
+            break;
+        /* yonghyuk */
+        case SYS_WAIT:
+            syscall_wait(*(pid_t *)args[0]);
+            break;
+        case SYS_READ:
+            sysca_read (*(int *)args[0], args[1] , *(size_t *)args[2]);
+            break;
+        case SYS_WRITE:
+            syscall_write(*(int *)args[0], args[1], *(size_t *)args[2]);
+            break;
+        default:
+            break;
+    }
     handle(syscallnum, f);
-    
     thread_exit ();
 }
 
@@ -73,7 +101,7 @@ syscall_exit (int status){
  * arguments and returns the new process's program id(pid).
  * Must return pid -1. */
 pid_t
-exec (const char *cmd_line){
+syscall_exec (const char *cmd_line){
     
 }
 
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -5,11 +5,11 @@ const int READ_FROM_KEYBORAD = 0;
 const int WRITE_TO_CONSOLE = 1;
 
 void syscall_init (void);
-
+void syscall_get_args(void *esp, void *args[], int syscallnum);
 /* jimin */
 void syscall_halt (void);
 void syscall_exit (int status);
-pid_t exec (const char *cmd_line);
+pid_t syscall_exec (const char *cmd_line);
 
 /* yonghyuk */
 int syscall_wait(pid_t pid);
